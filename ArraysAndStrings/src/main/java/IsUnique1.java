3829fdd0543f7cb303db4f4cc8d4ad045742a5b0
AnettKeszler <anett.dr.keszler@gmail.com>
2021-03-14 23:10:04 +0100

    solve Is Unique - brute force algorithm without using String array

--- ArraysAndStrings/src/main/java/IsUnique1.java
+++ ArraysAndStrings/src/main/java/IsUnique1.java
@@ -2,6 +2,7 @@
 // What if you cannot use additional data structure?
 
 public class IsUnique1 {
+    // brute force solution --> O(N**2)
     public boolean isUnique(String word) {
         String[] wordArray = word.split("");
 
@@ -9,6 +10,19 @@ public class IsUnique1 {
             for (int j = i+1; j < wordArray.length; j++) {
                 System.out.println(wordArray[i] + wordArray[j]);
                 if (wordArray[i].equals(wordArray[j])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    // brute force solution without String array
+    public boolean isUnique2(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            for (int j = i+1; j < word.length(); j++) {
+                if (word.charAt(i) == word.charAt(j)) {
                     return false;
                 }
             }
