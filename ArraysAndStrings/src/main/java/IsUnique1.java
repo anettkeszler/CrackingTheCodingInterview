6a5006c727d4d15cf11234620304591bc698e2cd
AnettKeszler <anett.dr.keszler@gmail.com>
2021-03-14 23:38:01 +0100

    solve Is Unique - with ASCII chars for more better time complexity

--- ArraysAndStrings/src/main/java/IsUnique1.java
+++ ArraysAndStrings/src/main/java/IsUnique1.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 // Implement an algorithm to determine if a string has all unique characters.
 // What if you cannot use additional data structure?
 
-public class IsUnique1 {
+public class IsUniqueBruteForce {
 
     // brute force solution: Run 2 loops with variable i and j. Compare str[i] and str[j].
     // If they become equal at any point, return false.
@@ -25,7 +25,7 @@ public class IsUnique1 {
 
     // brute force solution without String array
     // time complexity: O(N**2)
-    public boolean isUnique2(String word) {
+    public boolean isUniqueBruteForce2(String word) {
         for (int i = 0; i < word.length(); i++) {
             for (int j = i+1; j < word.length(); j++) {
                 if (word.charAt(i) == word.charAt(j)) {
@@ -38,7 +38,7 @@ public class IsUnique1 {
 
     // sorting: using sorting, based on ASCII values of characters
     // time complexity: O(N logN)
-    public boolean isUnique3(String word) {
+    public boolean isUniqueSorting(String word) {
         char[] wordArray = word.toCharArray();
         Arrays.sort(wordArray);
         for (int i = 0; i < wordArray.length -1 ; i++) {
@@ -51,5 +51,27 @@ public class IsUnique1 {
         return true;
     }
 
+    // This approach assumes ASCII char set(8 bits). The idea is to maintain a boolean array for the characters.
+    // The 256 indices represent 256 characters. All the array elements are initially set to false.
+    // As we iterate over the string, set true at the index equal to the int value of the character.
+    // If at any time, we encounter that the array value is already true, it means the character with that int value is repeated.
+    //Time Complexity: O(N)
+    public boolean isUniqueASCII(String word) {
+        int MAX_CHAR = 256;
 
+        if (word.length() > MAX_CHAR) {
+            return false;
+        }
+        boolean[] chars = new boolean[MAX_CHAR];
+        Arrays.fill(chars, false);
+
+        for (int i = 0; i < word.length(); i++) {
+            int index = (int)word.charAt(i);
+            if (chars[index] == true) {
+                return false;
+            }
+            chars[index] = true;
+        }
+        return true;
+    }
 }
